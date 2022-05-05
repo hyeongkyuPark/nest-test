b61afeedd5423e1dd01b9f96316a2d7f9c207ed4
소닉 <phg0644@slogup.com>
2022-05-05 11:55:14 +0900

    set cors config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(8080);
 }
 bootstrap();
